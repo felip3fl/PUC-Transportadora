390ff88bb545e585c1b1181d87c02c3fdf4c82ce
Felip3FL <felip3.fl@gmail.com>
2016-09-22 21:24:52 -0300

    Correção

--- src/a/transportadora/Transportadora.java
+++ src/a/transportadora/Transportadora.java
@@ -32,11 +32,11 @@ public class Transportadora {
 			Frota frota = new Frota();
 			
 			if (dias > 2){
-				Caminhao caminhao1 = new Caminhao(capacidadeDiariaTransporte / numeroMaximoCaminhao);
+				Caminhao caminhao1 = new Caminhao(capacidadeDiariaTransporte );
 				frota.adiciona(caminhao1);
 			}else{ 
-				Caminhao caminhao1 = new Caminhao(capacidadeDiariaTransporte / numeroMaximoCaminhao);
-				Caminhao caminhao2 = new Caminhao(capacidadeDiariaTransporte / numeroMaximoCaminhao);
+				Caminhao caminhao1 = new Caminhao(capacidadeDiariaTransporte );
+				Caminhao caminhao2 = new Caminhao(capacidadeDiariaTransporte);
 				frota.adiciona(caminhao1);
 				frota.adiciona(caminhao2);
 			}
