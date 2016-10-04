f6e6b5bc35ff1e2bbca60170b31ee83b7cbd6e61
Felip3FL <felip3.fl@gmail.com>
2016-10-04 15:02:01 -0300

    JUNIT Teste CalculoDiasTransporte

---
+++ src/a/testes/CalculoDiasTransporte.java
@@ -0,0 +1,19 @@
+package a.testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculoDiasTransporte {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testCalculoDiasTransporte() {
+		fail("Not yet implemented");
+	}
+
+}
