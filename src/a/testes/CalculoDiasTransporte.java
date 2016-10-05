f1930246a1a66168f8a2689fc95bd486b69729ad
Felip3FL <felip3.fl@gmail.com>
2016-10-04 23:37:01 -0300

    JUNIT Atualizacao

--- src/a/testes/CalculoDiasTransporte.java
+++ src/a/testes/CalculoDiasTransporte.java
@@ -5,15 +5,21 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class CalculoDiasTransporte {
+import a.transportadora.Transportadora;
 
+public class CalculoDiasTransporte {
+	
+	Transportadora transp; 
+	
 	@Before
 	public void setUp() throws Exception {
+		transp = new Transportadora();
+		transp.setCapacidadeDiariaTransporte(100);
 	}
 
 	@Test
 	public void testCalculoDiasTransporte() {
-		fail("Not yet implemented");
+		assertEquals(transp.calculoDiasTransporte(100), transp.calculoDiasTransporte(100));
 	}
 
 }
